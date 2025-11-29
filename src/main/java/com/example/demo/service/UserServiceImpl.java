0d93a097ce702b6ffe59919227c568aa4b37719e
marious93 <marious_93@mail.ru>
2025-11-30 01:06:01 +1000

    Second commit. Added persistenceContext for EntityManager

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
 
-import com.example.demo.dao.UserRepository;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
