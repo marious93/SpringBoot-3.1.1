b4ee5b5aef9c2e0ee6afaf97d4b71bfe2f7da922
marious93 <marious_93@mail.ru>
2025-12-03 00:06:30 +1000

    Second commit. Added persistenceContext for EntityManager

---
+++ src/main/java/com/example/demo/service/CustomAuthencationProvider.java
@@ -0,0 +1,51 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.MyUser;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthencationProvider implements AuthenticationProvider {
+
+    private final UserRepository dao;
+
+    public CustomAuthencationProvider(UserRepository dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        //получаем пользователя
+        MyUser myUser = dao.findByLogin(userName);
+        //смотрим, найден ли пользователь в базе
+        if (myUser == null) {
+            throw new BadCredentialsException("Unknown user " + userName);
+        }
+        if (!password.equals(myUser.getPassword())) {
+            throw new BadCredentialsException("Bad password");
+        }
+        UserDetails principal = User.builder()
+                .username(myUser.getLogin())
+                .password(myUser.getPassword())
+                .roles(myUser.getRole())
+                .build();
+        return new UsernamePasswordAuthenticationToken(
+                principal, password, principal.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.MyUser;
+import com.example.demo.repository.UserRepository;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        MyUser myUser= userRepository.findByLogin(userName);
+        if (myUser == null) {
+            throw new UsernameNotFoundException("Unknown user: "+userName);
+        }
+        UserDetails user = User.builder()
+                .username(myUser.getLogin())
+                .password(myUser.getPassword())
+                .roles(myUser.getRole())
+                .build();
+        return user;
+    }
+
+}
